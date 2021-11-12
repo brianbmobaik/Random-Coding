80866f39733c0de965ec9a5fe1456e69f7a789fe
brianbmobaik <89275270+brianbmobaik@users.noreply.github.com>
2021-11-12 12:15:11 -0800

    Update Binary.cpp

--- Binary.cpp
+++ Binary.cpp
@@ -2,21 +2,27 @@
 #include <iostream>
 #include <vector>
 
+// Constant
 const int TWO = 2;
 
+// Function Prototype
 std::vector<int> binaryVector(int wholeNumber);
 
 int main()
 {
+    // Variables
     int decimal;
     std::vector<int> binary;
-    std::string userPrompt = "Please enter a number in decimal: ";
+    std::string userPrompt = "Please enter an integer number in decimal: ";
 
+    // Prompt user to input an integer
     std::cout << userPrompt;
     std::cin >> decimal;
 
+    // Convert the input integer to binary
     binary = binaryVector(decimal);
 
+    // Output the Binary
     for(int i = 0; i < binary.size(); i++)
     {
         std::cout << binary[binary.size() - i - 1];
@@ -25,14 +31,20 @@ int main()
     std::cout << std::endl;
 }
 
+// This function converts an integer to binary
 std::vector<int> binaryVector(int wholeNumber)
 {
+    // Variable
     std::vector<int> binary;
+    
+    // Converts given integer to binary
     while(wholeNumber != 0)
     {
+        // Push back each remainder
         binary.push_back(wholeNumber % TWO);
         wholeNumber /= TWO;
     }
 
+    // Return the binary
     return binary;
 }
